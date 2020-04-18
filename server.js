9728833a8d77683b53f3ef77ff3bd3297f079eb6
Adam Owada <adamowada@gmail.com>
2020-04-18 15:42:08 -0700

    fixed error handling, updated weather date in m/d/y, added trails api

--- server.js
+++ server.js
@@ -18,37 +18,25 @@ app.use(cors());
 app.get('/location', handleLocation);
 
 function handleLocation( request, response ) {
-  try {
-    let city = request.query.city;
-    // eventually, get this from a real live API
-    // But today, pull it from a file.
-
-    // throw 'john is ugly or something';
-
-    const url = 'https://us1.locationiq.com/v1/search.php';
-    const queryStringParams = {
-      key: process.env.LOCATION_TOKEN,
-      q: city,
-      format: 'json',
-      limit: 1,
-    };
-
-    superagent.get(url)
-      .query(queryStringParams)
-      .then( data => {
-        let locationData = data.body[0];
-        // console.log(locationData);
-        let location = new Location(city, locationData);
-        response.json(location);
-      });
-  }
-  catch(error) {
-    let errorObject = {
-      status: 500,
-      responseText: 'john is ugly or something',
-    };
-    response.status(500).json(errorObject);
-  }
+  const city = request.query.city;
+  const url = 'https://us1.locationiq.com/v1/search.php';
+  const queryStringParams = {
+    key: process.env.LOCATION_TOKEN,
+    q: city,
+    format: 'json',
+    limit: 1,
+  };
+  superagent.get(url)
+    .query(queryStringParams)
+    .then( data => {
+      let locationData = data.body[0];
+      let location = new Location(city, locationData);
+      response.json(location);
+    })
+    .catch(error => {
+      console.log(error);
+      response.status(500).send('Bad Location Request');
+    });
 }
 
 function Location(city, data) {
@@ -62,42 +50,63 @@ function Location(city, data) {
 app.get('/weather', handleWeather);
 
 function handleWeather(request, response) {
-  try {
-
-    // use darksky fake data
-    // eventually will be an api call
-    // let weatherData = require('./data/darksky.json');
-
-    let url = 'https://api.darksky.net/forecast/';
-    let key = process.env.DARKSKY_TOKEN;
-    let lat = request.query.latitude;
-    let lon = request.query.longitude;
-
-    // user-key
-    let newUrl = `${url}${key}/${lat},${lon}`;
-
-    superagent.get(newUrl)
-      .then( data => {
-        let listOfDays = data.body.daily.data.map( day => {
-          return new Weather(day);
-        })
-        response.json(listOfDays);
-      }).catch( error => {
-        console.log(error);
-      });
-  }
-  catch(error) {
-    let errorObject = {
-      status: 500,
-      responseText: 'john is ugly or something',
-    };
-    response.status(500).json(errorObject);
-  }
+  const url = 'https://api.darksky.net/forecast/';
+  const key = process.env.DARKSKY_TOKEN;
+  const lat = request.query.latitude;
+  const lon = request.query.longitude;
+  const newUrl = `${url}${key}/${lat},${lon}`;
+  superagent.get(newUrl)
+    .then( data => {
+      const listOfDays = data.body.daily.data.map( day => {
+        return new Weather(day);
+      })
+      response.json(listOfDays);
+    })
+    .catch( error => {
+      console.log(error);
+      response.status(500).send('Bad Weather Request');
+    });
 }
 
+
 function Weather(data) {
-  this.time = data.time; // format to date (not epoch)
+  this.time = `${new Date(data.time * 1000).getMonth()+1}-${new Date(data.time * 1000).getDate()}-${new Date(data.time * 1000).getFullYear()}` ; // format to date (not epoch)
   this.forecast = data.summary;
+}
+
+
+app.get('/trails', handleTrails);
+function handleTrails( request, response ) {
+  const url = 'https://www.hikingproject.com/data/get-trails';
+  const queryStringParams = {
+    lat: request.query.latitude,
+    lon: request.query.longitude,
+    key: process.env.TRAIL_TOKEN,
+  };
+  superagent.get(url)
+    .query(queryStringParams)
+    .then( data => {
+      console.log(data.body.trails);
+      const listOfTrails = data.body.trails.map( trail => new Trails(trail) );
+      response.json(listOfTrails);
+    })
+    .catch(error => {
+      console.log(error);
+      response.status(500).send('Bad Trails Request');
+    });
+}
+
+function Trails(data) {
+  this.name = data.name;
+  this.location = data.location;
+  this.length = data.length;
+  this.stars = data.stars;
+  this.star_votes = data.starVotes;
+  this.summary = data.summary;
+  this.trail_url = data.url;
+  this.conditions = data.conditionDetails;
+  this.condition_date = data.conditionDate.match(/\d\d\d\d-\d\d-\d\d/);
+  this.condition_time = data.conditionDate.match(/\d\d:\d\d:\d\d/);
 }
 
 
